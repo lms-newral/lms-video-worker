f408d6f32c165804cc92272d26695aa645adb7a5
Ram Bhardwaj <rambhardwaj101000@gmail.com>
2026-02-12 12:59:31 +0530

    feat:added envs

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,5 +1,4 @@
-import dotenv from "dotenv"
-dotenv.config();
+import 'dotenv/config';
 
 export const redisConfig = {
   host: process.env.REDIS_HOST || 'localhost',
--- src/config/s3.ts
+++ src/config/s3.ts
@@ -1,11 +1,11 @@
 import { S3Client } from '@aws-sdk/client-s3';
-import dotenv from "dotenv"
-
-dotenv.config();
+import 'dotenv/config';
 
 console.log("In S3")
 console.log("AWS Access Key ID:", process.env.AWS_ACCESS_KEY_ID!)
 console.log("AWS Secret Access Key:", process.env.AWS_SECRET_ACCESS_KEY!)
+console.log("Region", process.env.AWS_REGION!)
+console.log("BE URL", process.env.BACKEND_URL)
 
 export const s3Client = new S3Client({
   region: process.env.AWS_REGION || 'us-east-1',
